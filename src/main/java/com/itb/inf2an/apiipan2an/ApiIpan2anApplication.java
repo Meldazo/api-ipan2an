f219717b433d05caf997878221d22b55c9b5da7b
Domingos José dos Santos Neto <rm90659@estudante.fieb.edu.br>
2024-09-24 22:17:53 -0300

    incío controle de versão

---
+++ src/main/java/com/itb/inf2an/apiipan2an/ApiIpan2anApplication.java
@@ -0,0 +1,13 @@
+package com.itb.inf2an.apiipan2an;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiIpan2anApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiIpan2anApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itb/inf2an/apiipan2an/ApiIpan2anApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.inf2an.apiipan2an;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiIpan2anApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
